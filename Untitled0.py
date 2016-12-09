4d3ea66afe1211022c92b6e5e2bf0ec780e7f879
Arjun S Kumar <arjunskumar22@gmail.com>
2016-12-09 12:21:06 +0530

    Add files via upload
    Hand Writing recognition using SVM. 
    Dataset from sklearn.databases import load_digits

---
+++ Untitled0.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from sklearn.datasets import load_digits
+digits = load_digits()
+
+
+# In[2]:
+
+print type(digits)
+print digits.data
+
+
+# In[3]:
+
+print digits.DESCR
+
+
+# In[4]:
+
+print digits.target
+
+
+# In[5]:
+
+print digits.target_names
+
+
+# In[7]:
+
+print digits.target.shape
+print digits.data.shape
+
+
+# In[11]:
+
+X = digits.data
+y = digits.target
+
+
+# In[12]:
+
+print X
+print y
+
+
+# In[15]:
+
+from sklearn import svm
+clf = svm.SVC(gamma=0.001, C=100)
+clf.fit(X,y)
+print('Prediction:'), clf.predict(digits.data[12])
+print('Actual:'), y[12]
+
+
+# In[ ]:
+
+
+
